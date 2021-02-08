81f6bb7e8b006fcd04d0f57595151bc02fe23706
KimTed <bbielo20@gmail.com>
2021-02-09 00:27:22 +0900

    feat: vuex 작업 준비

---
+++ vueJS/vue_sandbox/README.md
@@ -0,0 +1,14 @@
+# 맨땅에 개발하기
+
+* TIL
+  * Vuex 공부...
+    ```
+    Vuex store is not a constructor
+    ```
+    > new Vuex.Store({}), 대문자 S
+  --- 
+  * git add -u || git rm [file]
+    ```
+    deleted:    ../LICENSE
+    ```
+    > deleted 상태 처리 방법
---
+++ vueJS/vue_sandbox/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">HOME</router-link>
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view />
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: "App",
+};
+</script>
+
+<style>
+</style>
