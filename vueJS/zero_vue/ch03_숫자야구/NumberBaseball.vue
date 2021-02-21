c0d7151168d0435de2d7e4108db0cf8172def8a4
KimTed <bbielo20@gmail.com>
2021-02-21 17:46:30 +0900

    feat: 숫자야구 _ 작업중

--- vueJS/zero_vue/ch03_숫자야구/NumberBaseball.vue
+++ vueJS/zero_vue/ch03_숫자야구/NumberBaseball.vue
@@ -3,7 +3,7 @@
         <h1>{{result}}</h1>
         <form @submit.prevent="onSubmitForm">
             <input maxlength="4" minlength="4" ref="answer" v-model="value" />
-            <button>입력</button>
+            <button type="submi">입력</button>
         </form>
         <div>시도: {{tries.length}}</div>
         <ul>
@@ -16,9 +16,22 @@
 </template>
 
 <script>
+const getRandomNum = () => {
+    const candidates = [1,2,3,4,5,6,7,8,9];
+    const arr = [];
+    for (let i = 0; i < 4; i++) {
+        const choosen = candidates.splice(
+            Math.floor(
+                Math.random() * (9 - i)
+            ), 1);
+        arr.push(choosen);
+    }
+    return arr;
+}
 export default {
     data() {
         return {
+            answers: getRandomNum(),
             tries:[],
             value: '',
             result: ''
